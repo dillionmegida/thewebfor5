955c43e9d45304e46c14cf9da3ea7e40bf1fb5c3
Dillion Megida <megidadillion@gmail.com>
2019-08-30 16:52:43 +0100

    Some changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+// import { Helmet } from 'react-helmet';
 
 export default (props) => (
     <div className='App'>
+    	// <Helmet>
+    	// 	<meta charSet="utf-8" />
+    	// 	<title>
+    	// 		{props.PageTitle}
+    	// 	</title>
+    	// 	<link rel="canonical" href=`http://mysite.com/{props.PageLink}` />
+    	// </Helmet>
         {props.children}
     </div>
 )
