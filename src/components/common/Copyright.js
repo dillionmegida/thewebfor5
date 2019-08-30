7fd4e449da65426bb002d27951e727fdaf55f4bf
Dillion Megida <megidadillion@gmail.com>
2019-08-30 12:35:28 +0100

    Adjusted Copyright

--- src/components/common/Copyright.js
+++ src/components/common/Copyright.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 let Copyright = (props) => (
     <p style={{color: props.color}}>
-        Copyright {new Date().getFullYear()}
+        <span style={{fontSize: '18px'}}>
+            Copyright {new Date().getFullYear()}
+        </span>
         <br/><br/>
-        Articles written with &nbsp;
-        <span style={{color: 'red', fontSize: '20px'}}>&hearts;</span> &nbsp;
-        by &nbsp;
+        Articles written with ❤️
+        by<br/>
         <a href='http://twitter.com/iamdillion' style={{color: props.color, fontWeight: 'bold', letterSpacing: '1px'}} title='Dillion Megida'>
             Dillion Megida
         </a>
