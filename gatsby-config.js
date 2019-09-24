bbf72fc1351cd27e2a2c92fe4fcd4d1810b30b74
Dillion Megida <megidadillion@gmail.com>
2019-09-24 13:02:04 +0100

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,17 +80,7 @@ module.exports = {
         anonymize: true,
         // Setting this parameter is also optional
         respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Any additional optional fields
+        
         sampleRate: 5,
         siteSpeedSampleRate: 10,
         cookieDomain: "dillionmegida.com",
