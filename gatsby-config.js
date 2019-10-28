2e0469514c86119daa19e1dbfcd92d2faa9f1cc6
Dillion <42855542+dillionmegida@users.noreply.github.com>
2019-10-29 00:02:23 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,3 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
   siteMetadata: {
     siteUrl: `https://dillionmegida.com`,
