8c14fc23f2da58f7b08aee0b2b8e6b0684a07470
dillionmegida <dillionmegida@gmail.com>
2019-12-20 13:49:14 +0100

    Under development

--- src/containers/Theme/Theme.js
+++ src/containers/Theme/Theme.js
@@ -1,39 +1,39 @@
-// import React from 'react'
-// import Styles from './Theme.module.scss';
+import React from 'react'
+import Styles from './Theme.module.scss';
 
 
-// import { ThemeToggler } from 'gatsby-plugin-dark-mode'
+import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 
-// export default class Theme extends React.Component {
-//   render() {
-//     return (
-//       <ThemeToggler>
-//         {({ theme, toggleTheme }) => (
-//           // <label>
-//           //   <input
-//           //     type="checkbox"
-//           //     onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
-//           //     checked={theme === 'dark'}
-//           //   />{' '}
-//           //   Dark mode
-//           // </label>
-//           <div className={Styles.toggle}>
-//             {/* <input 
-//               onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
-//               checked={theme === 'dark'}
-//               type="checkbox" 
-//             /> */}
-//               <input
-//                 onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
-//                 type="checkbox"
-//                 id="toggle"
-//                 checked={theme === 'light' ? false : true}
-//               />
-//               <label htmlFor="toggle"></label>
-//             {/* <label for="toggle"></label> */}
-//           </div>
-//         )}
-//       </ThemeToggler>
-//     )
-//   }
-// }
+export default class Theme extends React.Component {
+  render() {
+    return (
+      <ThemeToggler>
+        {({ theme, toggleTheme }) => (
+          // <label>
+          //   <input
+          //     type="checkbox"
+          //     onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
+          //     checked={theme === 'dark'}
+          //   />{' '}
+          //   Dark mode
+          // </label>
+          <div className={Styles.toggle}>
+            {/* <input 
+              onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
+              checked={theme === 'dark'}
+              type="checkbox" 
+            /> */}
+              <input
+                onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
+                type="checkbox"
+                id="toggle"
+                checked={theme === 'light' ? false : true}
+              />
+              <label htmlFor="toggle"></label>
+            {/* <label for="toggle"></label> */}
+          </div>
+        )}
+      </ThemeToggler>
+    )
+  }
+}
