2edbf67b66094c92d364600a92b3970257b2928d
dillionmegida <dillionmegida@gmail.com>
2019-12-20 21:35:41 +0100

    Include new plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,14 @@ module.exports = {
 
         // For NetlifyCMS
         `gatsby-plugin-netlify-cms`,
+
+        // Automatic sitemaps when built
+        {
+            resolve: `gatsby-plugin-sitemap`,
+            options: {
+            exclude: ["/tags/*", "/search", "/categories"]
+            }
+        },
         
         // React Helmet for populating thehead tag
         `gatsby-plugin-react-helmet`,
@@ -59,5 +67,23 @@ module.exports = {
                 ]
             }
         },
+
+        // For gatsby manifest
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+            name: `TheWebFor5`,
+            short_name: `TheWebFor5`,
+            start_url: `/`,
+            background_color: `#262625`,
+            theme_color: `#262625`,
+            display: `standalone`,
+            icon: `src/img/assets/logo.png`,
+            },
+        },
+    
+    
+        // Gatsby offline
+        `gatsby-plugin-offline`
     ]
 }
