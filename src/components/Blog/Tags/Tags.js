845d34f47fb8b3de87e705c08bc40307ca4151ff
dillionmegida <dillionmegida@gmail.com>
2019-12-20 21:41:06 +0100

    Limit tags

--- src/components/Blog/Tags/Tags.js
+++ src/components/Blog/Tags/Tags.js
@@ -24,18 +24,39 @@ export default () => (
         render={ data => {
             const { edges } = data.allMarkdownRemark
             console.log(edges);
+
+            const tagsArr = [];
+            const limit = 15;
+
+            edges.forEach(post => {
+                post.node.frontmatter.tags.forEach(tag => {
+                    if(tagsArr.length < limit && !tagsArr.includes(tag)) {
+                        tagsArr.push(tag);
+                    }
+                })
+            })
+
+            // console.log(tagArr);
+
             return (         
                 <React.Fragment>      
-                    { 
-                        edges.map(({ node }) => (
+                    {/* { 
+                        limitedArr.map(post => (
                             // check if the tags array is not empty, because articles may not have them
-                            node.frontmatter.tags !== null && node.frontmatter.tags.map((tag, index) => (
+                            post.frontmatter.tags !== null && post.frontmatter.tags.map((tag, index) => (
                                 <React.Fragment key={`${tag}_${index}`}>
                                     <Link to={`/tags/${tag}`} title={`Posts tagged with ${tag}`}>
                                         {tag}
                                     </Link>
                                 </React.Fragment>
                             ))
+                        ))
+                    } */}
+                    {
+                        tagsArr.map((tag, index) => (
+                            <Link key={`${tag}_${index}`} to={`/tags/${tag}`} title={`Posts tagged with ${tag}`}>
+                                {tag}
+                            </Link>
                         ))
                     }
                 </React.Fragment> 
