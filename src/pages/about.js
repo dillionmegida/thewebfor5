4625bbccfd0d897be6a4babc275039db4a20a2c3
dillionmegida <dillionmegida@gmail.com>
2019-12-21 18:28:28 +0100

    Use new components

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ import { graphql } from 'gatsby';
 import Layout from '../containers/Layout/Layout';
 import Brand from '../components/Brand/Details';
 import Founder from '../assets/img/dillion.jpg';
+import { TwitterMedia, WebsiteMedia } from '../components/SocialMedia/Media';
 import MainImg from '../assets/img/main.jpg';
 import SuggestArticles from '../components/Blog/SuggestArticles/SuggestArticles';
 
@@ -32,7 +33,7 @@ export default ({ data }) => {
                     <p>{Brand.name} is a blog platform for demystifying web topics as assumed to be teaching a five year old. It was launched by {Brand.founder} on January 1st, 2020 (more details on him below).</p>
                     <p>So far, there are <b>{noOfArticles}</b> articles already written.</p>
                     <p>There are undeniably numerous platforms already built for teaching web topics but this hopes to be different such that the topics aren't entirely explained in technical terms but simpler ones which a five year old would understand. Also note that despite the means used in explanation, the technicalities involved in such topics would not be left behind.</p>
-                    <p>The little requirement for five year olds (lol) hoping to learn from this platform is that they should have the appetite for reading. This is stated because literally, five year olds wouldn't actually like reading. Illustrations would be used. Stories would also be used.</p>
+                    <p>The little requirement for five year olds (lol) hoping to learn from this platform is that they should have the appetite for reading. This is stated because literally, five year olds wouldn't actually like reading. Right? Illustrations would be used. Stories would also be used.</p>
                     <p>We hope to give you the best in this platform : )</p>
                 </div>
 
@@ -46,12 +47,14 @@ export default ({ data }) => {
                             A front-end developer, graphics designer and technical writer. He founded this site with the purpose of breaking down topics in web development into its simplest pieces.
                         </p>
                         <div className={Styles.Links}>
-                            <a href='https://dillionmegida.com' title={`${Brand.founder}'s Website`}>
-                                <i className='fa fa-link'></i>
-                            </a> &nbsp;
-                            <a href='https://twitter.com/iamdillion' title={`${Brand.founder}'s Twitter handle`}>
-                                <i className='fa fa-twitter'></i>
-                            </a>
+                            <WebsiteMedia
+                                href={Brand.founder.website}
+                                title={`${Brand.founder.name}'s Website`}
+                            /> &nbsp;
+                            <TwitterMedia
+                                href={Brand.founder.twitter}
+                                title={`${Brand.founder.name}'s Twitter handle`}
+                            />
                         </div>
                     </div>
                 </div>
