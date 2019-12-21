5a46a1e9579dd8db014eec6a2ed0a6c8118236e4
dillionmegida <dillionmegida@gmail.com>
2019-12-21 18:26:14 +0100

    Add media

---
+++ src/components/SocialMedia/Media.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TwitterMedia = props => (
+    <a href={props.href} title={props.title}>
+        <i className='fa fa-twitter'></i>
+    </a>
+)
+
+const WebsiteMedia = props =(
+    <a href={props.href} title={props.title}>
+        <i className='fa fa-link'></i>
+    </a>
+)
+
+export { TwitterMedia, WebsiteMedia }
--- src/components/SocialMedia/ShareArticle.js
+++ src/components/SocialMedia/ShareArticle.js
@@ -3,13 +3,13 @@ import Styles from './ShareArticle.module.scss';
 
 import Brand from '../Brand/Details';
 
-const Twitter = props => {
+const ShareTwitter = props => {
     const text = `${props.articleTitle} on @${Brand.twitter} - ${Brand.domain}${props.href} %23${Brand.name.toLowerCase()}`;
     return (
         <a className={Styles.Link} target='_blank' rel='noopener noreferrer' href={`https://twitter.com/intent/tweet?text=${text}`} >
-            <i style={{color: '#1DA1F2'}} className='fa fa-twitter'></i>T
+            <i style={{color: '#1DA1F2'}} className='fa fa-twitter'></i>
         </a>
     )
 }
 
-export { Twitter };
+export { ShareTwitter };
