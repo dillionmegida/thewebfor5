6539e3b54e83b721cfec85fc09c2ab95492899dd
dillionmegida <dillionmegida@gmail.com>
2019-12-21 18:03:12 +0100

    Add google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,6 +68,19 @@ module.exports = {
             }
         },
 
+        // For google analytics
+        {
+            resolve: `gatsby-plugin-gtag`,
+            options: {
+            // your google analytics tracking id
+            trackingId: "UA-148541646-3",
+            // Puts tracking script in the head instead of the body
+            head: true,
+            // enable ip anonymization
+            anonymize: true,
+            },
+        },
+
         // For gatsby manifest
         {
             resolve: `gatsby-plugin-manifest`,
