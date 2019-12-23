17d966e14a5865990841cb39b9562da91bfb32fb
Dillion <42855542+dillionmegida@users.noreply.github.com>
2019-12-23 16:26:58 +0100

    Update Layout.js

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -16,18 +16,6 @@ import Drawer from '../../components/Nav/Drawer/Drawer';
 // }
 
 export default class Layout extends React.Component {
-    state = {
-        showTheme: false
-    }
-
-    componentDidMount(){
-        setTimeOut(() => {
-            this.setState({
-                showTheme: true
-            })
-        }, 500)
-    }
-    
     render() {
 
         return (
@@ -58,11 +46,7 @@ export default class Layout extends React.Component {
                     <Link to='/saved' title='Saved Articles' className={Styles.Saved} activeClassName={Styles.SavedActive}>
                         <i className='fa fa-bookmark'></i>
                     </Link>
-                    {/* The theme handler is always checked before transiting to uncheck when light theme is chosen
-                     so I chose to show it when the component mounts - few seconds later, so the transition doesn't show */}
-                    <span style={{visibility: this.state.showTheme ? 'visible' : 'hidden'}}>
-                        <PageTheme />
-                    </span>
+                    <PageTheme />
                 </div>
 
                 <main className={Styles.BodyContent}>
