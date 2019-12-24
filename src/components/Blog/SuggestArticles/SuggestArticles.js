733b7acfff08195948dec4628c63a93424b13dc5
dillionmegida <dillionmegida@gmail.com>
2019-12-24 02:08:09 +0100

    Add github issue

--- src/components/Blog/SuggestArticles/SuggestArticles.js
+++ src/components/Blog/SuggestArticles/SuggestArticles.js
@@ -5,7 +5,7 @@ export default () => (
     <div className={Styles.Suggest}>
         <p>
             Do you have any topic in web development which you would love to be written about?<br/>
-            If yes, kindly <a href='https://forms.gle/5Npd7mdWJwiqpnreA' target='_blank' rel='noopener noreferrer' title='Suggest articles on this form'>fill this form</a>.
+            If yes, kindly <a target='_blank' rel='noopener noreferer' href='https://github.com/dillionmegida/thewebfor5/issues/new' title='Create new issue'>create a new issue on the repository</a> or <a href='https://forms.gle/5Npd7mdWJwiqpnreA' target='_blank' rel='noopener noreferrer' title='Suggest articles on this form'>fill this form</a>.
         </p>
     </div>
 )
