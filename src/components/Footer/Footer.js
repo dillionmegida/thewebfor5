6915cbdff264a82eb3563c950b309107658db9a2
dillionmegida <dillionmegida@gmail.com>
2019-12-26 02:39:19 +0100

    Adjust footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,16 +3,20 @@ import Styles from './Footer.module.scss';
 
 import { Link } from 'gatsby';
 import Brand from '../Brand/Details';
+import { TwitterMedia } from '../SocialMedia/Media';
 
 export default () => {
     return (
         <footer className={Styles.Footer}>
             <p className={Styles.Copyright}>
-                Copyright &copy;&nbsp;
-                <Link to='/' title={Brand.name}>
-                    {Brand.name}
-                </Link>
+                &copy; {new Date().getFullYear()}
             </p>
+            | <span className={Styles.SocialMedia}>
+                <TwitterMedia
+                    href={Brand.twitter}
+                    title={`${Brand.name} Titter Handle`}
+                />
+            </span>
             | <a className={Styles.Repo} href={Brand.repo} title={`GitHub repository for ${Brand.name}`}>Contribute <i className='fa fa-github'></i></a>
         </footer>
     )
