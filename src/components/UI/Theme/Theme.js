73f9308240c755ca0b675b5f978be92aca88ffc9
dillionmegida <dillionmegida@gmail.com>
2019-12-28 19:51:17 +0100

    Configure personal theme

---
+++ src/components/UI/Theme/Theme.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Styles from './Theme.module.scss';
+
+import Brand from '../../Brand/Details';
+import { checkGlobal } from '../../Blog/Saved/Saved';
+import { Sun, Moon } from '../Icons'; 
+
+const storageKey = `${Brand.name}_Theme`;
+
+// initialize theme
+if(checkGlobal()) {
+    let prevTheme = window.localStorage.getItem(storageKey);
+    if(prevTheme === null) {
+        window.localStorage.setItem(storageKey, 'light');
+    }
+}
+
+console.log(window.localStorage.getItem(storageKey));
+
+const saveDark = () => {
+    if(checkGlobal()) {
+        window.localStorage.setItem(storageKey, 'dark');
+    }
+}
+
+const saveLight = () => {
+    if(checkGlobal()) {
+        window.localStorage.setItem(storageKey, 'light');
+    }
+}
+
+const isThemeLight = () => {
+    if(checkGlobal()) {
+        let currentTheme = window.localStorage.getItem(storageKey);
+        return currentTheme === 'light'
+    }
+}
+
+export default props => {
+
+    return (
+        <React.Fragment>
+            {
+                isThemeLight() ?
+                    <button className={Styles.DarkBtn} onClick={props.changeTheme}>
+                        <Moon />
+                    </button>
+                :
+                    <button className={Styles.LightBtn} onClick={props.changeTheme}>
+                        <Sun />
+                    </button>
+            }
+        </React.Fragment>
+    )
+}
+
+export { saveDark, saveLight, isThemeLight };
