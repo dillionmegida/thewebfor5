363f644a2f9add6c8129e6316b14b772856800ca
dillionmegida <dillionmegida@gmail.com>
2019-12-28 19:53:56 +0100

    Theme icons

--- src/components/UI/Icons.js
+++ src/components/UI/Icons.js
@@ -6,17 +6,25 @@ const Icon = props => (
     </Fragment>
 )
 
-const BackArrow = props => (
+const BackArrow = () => (
     <Icon class='fa fa-chevron-left' />
 )
 
-const Link = props => (
+const Link = () => (
     <Icon class='fa fa-link' />
 )
 
+// Theme
+const Sun = () => (
+    <Icon class='fa fa-sun-o' />
+)
+const Moon = () => (
+    <Icon class='fa fa-moon-o' />
+)
+
 // Social Media Icons
-const Twitter = props => (
+const Twitter = () => (
     <Icon class='fa fa-twitter' />
 )
 
-export { BackArrow, Link, Twitter };
+export { BackArrow, Link, Twitter, Sun, Moon };
