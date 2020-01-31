e2180aadcb34e57758706d6a617457c166b45dde
dillionmegida <dillionmegida@gmail.com>
2020-01-31 17:27:38 +0100

    Sort posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,7 +19,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     const { createPage } = actions;
     const result = await graphql(`
         query {
-            allMarkdownRemark {
+            allMarkdownRemarksort: { fields: [frontmatter___date], order: DESC } {
                 edges {
                     node {
                       id
