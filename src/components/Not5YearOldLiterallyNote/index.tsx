214a67edffb2c4d2a5d26b686d5e10173274f561
dillionmegida <dillionmegida@gmail.com>
2021-02-03 14:16:51 +0100

    minor changes

--- src/components/Not5YearOldLiterallyNote/index.tsx
+++ src/components/Not5YearOldLiterallyNote/index.tsx
@@ -7,35 +7,36 @@ const Container = styled.div`
   padding: 20px;
   border-radius: 5px;
   border: 1px solid var(--color2);
+  h1 {
+    font-weight: bold;
+    margin: 0;
+    font-size: 18px;
+    display: block;
+    margin-bottom: 10px;
+    position: relative;
+    overflow: hidden;
+    &::after {
+      content: "";
+      height: 1px;
+      background-color: var(--color5);
+      width: 100%;
+      position: absolute;
+      left: 0;
+      top: 0;
+      margin-top: 13px;
+      margin-left: 100px;
+    }
+  }
   p {
     margin: 0;
-    span {
-      font-weight: bold;
-      font-size: 18px;
-      display: block;
-      margin-bottom: 5px;
-      position: relative;
-      overflow: hidden;
-      &::after {
-          content: '';
-        height: 1px;
-        background-color: var(--color5);
-        width: 100%;
-        position: absolute;
-        left: 0;
-        top: 0;
-        margin-top: 13px;
-        margin-left: 100px;
-      }
-    }
   }
 `
 
 export default function Not5YearOldLiterallyNote() {
   return (
     <Container>
+      <h1>Welcome</h1>
       <p>
-        <span>Welcome</span>
         We do not expect an actual 5 year old on this platform. The term
         "WebFor5" connotes simplicity in our articles. We ensure that our
         articles on various web development topics are easy to understand and
